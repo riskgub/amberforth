62b506f73d2fab81e9bfe59ca777fa1c01609be7
riskgub <124504346+riskgub@users.noreply.github.com>
2023-06-07 08:45:18 +0200

    Update abstacks.fs

--- lib/abstacks.fs
+++ lib/abstacks.fs
@@ -1,4 +1,4 @@
-64 CONSTANT STACK-SIZE
+64 CELLS CONSTANT STACK-SIZE
 CREATE astack STACK-SIZE CELLS ALLOT
 CREATE bstack STACK-SIZE CELLS ALLOT
 VARIABLE sap VARIABLE sbp
@@ -6,11 +6,11 @@ astack STACK-SIZE + CONSTANT sa0
 bstack STACK-SIZE + CONSTANT sb0
 sa0 sap ! sb0 sbp !
 
-: sap@ ( -- n ) sap @ ;
-: sbp@ ( -- n ) sbp @ ;
-: >A ( n -- ) 1 CELLS NEGATE sap +! sap @ ! ;
-: A> ( -- n ) sap @ @ 1 CELLS sap +! ;
-: A@ ( -- n ) sap @ @ ;
-: >B ( n -- ) 1 CELLS NEGATE sbp +! sbp @ ! ;
-: B> ( -- n ) sbp @ @ 1 CELLS sbp +! ;
-: B@ ( -- n ) sbp @ @ ;
+: sap@ (   -- n ) sap @ ;
+: sbp@ (   -- n ) sbp @ ;
+: >A   ( n --   ) 1 CELLS NEGATE sap +! sap @ ! ;
+: A>   (   -- n ) sap @ @ 1 CELLS sap +! ;
+: A@   (   -- n ) sap @ @ ;
+: >B   ( n --   ) 1 CELLS NEGATE sbp +! sbp @ ! ;
+: B>   (   -- n ) sbp @ @ 1 CELLS sbp +! ;
+: B@   (   -- n ) sbp @ @ ;
